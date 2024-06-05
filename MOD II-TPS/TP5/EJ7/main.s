de14dfe8cad7b4e1b074275ee53d5e78091a392b
juanSoler04 <164801034+juanSoler04@users.noreply.github.com>
2024-06-05 11:53:09 -0300

    tps CAC

--- MOD II-TPS/TP5/EJ7/main.s
+++ MOD II-TPS/TP5/EJ7/main.s
@@ -3,3 +3,30 @@
 ;La subrutina debe retornar la cantidad de valores mayores que M contenidos en la tabla.
 
 .data
+numM: .word 7
+tabla: .word 2,5,8,9,4
+dim: .word 5
+mayoresM: .word 0
+
+.code
+ld $a0,numM(r0)
+ld $a1,tabla(r0)
+ld $a2,dim(r0)
+jal maximos              ;copia en r31 la direc de retorno 
+sd $v0,mayoresM(r0)
+halt
+
+maximos: dadd $v0,$v0,r0   ;lo ini
+dadd $t0,$t0,r0    ;para mov en la tabla
+loop: slt $t1,$a0,$a1       ;deja 1 si a1 > a0 -- tabla > M  y sino 0
+dadd $v0,$v0,$t1   ;si no es mayor le sumara 0
+daddi $t0,$t0,8   ;sig elem
+daddi $a2,$a2,-1
+ld $a1,tabla($t0)
+bnez $a2,loop
+jr $ra    ;vuelve al main 
+
+
+;preg si conviene usar branch target buffer o no
+;BTB: 1.282 CPI
+;sin BTB: 1.256 CPI
