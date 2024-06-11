1aa0fbfc4db23049bc6215d85ed83c2ae64b2959
juanSoler04 <164801034+juanSoler04@users.noreply.github.com>
2024-06-11 18:09:51 -0300

    tps CAC

--- MOD II-TPS/TP6/EJ1/mains.s
+++ MOD II-TPS/TP6/EJ1/mains.s
@@ -1,15 +1,27 @@
 .data
-texto: .asciiz "Hola, Mundo!" ; El mensaje a mostrar
+texto: .asciiz  ; El mensaje a mostrar
 CONTROL: .word32 0x10000
 DATA: .word32 0x10008
 
 .text
 lwu $s0, DATA(r0) ; $s0 = dirección de DATA
+lwu $s1, CONTROL(r0) ; $s1 = dirección de CONTROL
+
+;uso control 9 para leer un msj en la direc texto
+loop: daddi $t0,$0,9
+sd $t0, 0($s1) ; CONTROL recibe 9
+lbu $t1,0($s0)    ;tomo el caract de data
+sd $t1,texto($0)   ;lo guardo en texto
+;daddi $t3,$0,1
+
 daddi $t0, $0, texto ; $t0 = dirección del mensaje a mostrar
 sd $t0, 0($s0) ; DATA recibe el puntero al comienzo del mensaje
-lwu $s1, CONTROL(r0) ; $s1 = dirección de CONTROL
-daddi $t0, $0, 6 ; $t0 = 6 -> función 6: limpiar pantalla alfanumérica
-sd $t0, 0($s1) ; CONTROL recibe 6 y limpia la pantalla
+
+;daddi $t0, $0, 6 ; $t0 = 6 -> función 6: limpiar pantalla alfanumérica
+;sd $t0, 0($s1) ; CONTROL recibe 6 y limpia la pantalla
+
 daddi $t0, $0, 4 ; $t0 = 4 -> función 4: salida de una cadena ASCII
 sd $t0, 0($s1) ; CONTROL recibe 4 y produce la salida del mensaje
+j loop
+;al no tener cond de corte seguira imp infinitamente 
 halt
